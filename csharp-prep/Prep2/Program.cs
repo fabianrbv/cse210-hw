432ac8de2fffa1c0020cbc2e787be02a474e32cc
fabianrbv <fabianrbv@gmail.com>
2023-01-07 23:16:23 -0500

    new comment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello, What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int porcentage = int.Parse(answer);
+
+        string letter = "";
+
+        if (porcentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (porcentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (porcentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (porcentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (porcentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
