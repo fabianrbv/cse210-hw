78bc5ea106af7b0031045cebfdc2cb392895abec
fabianrbv <fabianrbv@gmail.com>
2023-02-25 22:16:58 -0500

    new

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -10,19 +10,19 @@ class Program
         // string _bottom = Console.ReadLine();
         
         Fraction f1 = new Fraction();
-        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetFractionString("1", "0"));
         Console.WriteLine(f1.GetDecimalValue());
 
         Fraction f2 = new Fraction(5);
-        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetFractionString("5", "0"));
         Console.WriteLine(f2.GetDecimalValue());
 
         Fraction f3 = new Fraction(3, 4);
-        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetFractionString("3","4"));
         Console.WriteLine(f3.GetDecimalValue());
 
         Fraction f4 = new Fraction(1, 3);
-        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetFractionString("1","3"));
         Console.WriteLine(f4.GetDecimalValue());
     }
 }
