dfc286dd5056b1893ee45707c26ad8e87b15de7f
Fabian Betancourt <fabianrbv@gmail.com>
2023-12-13 22:10:47 -0500

    3

--- final/FinalProject/Program1Abstraction/Program.cs
+++ final/FinalProject/Program1Abstraction/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-internal class Program
+internal class Program1
 {
     static void Main()
     {
--- final/FinalProject/Program2Encapsulation/Program.cs
+++ final/FinalProject/Program2Encapsulation/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-internal class Program
+internal class Program2
 {
     static void Main()
     {
